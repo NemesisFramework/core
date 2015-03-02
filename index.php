8377d452c18fc571d0309c2ab44f0d022482220d
kimihub <kimhimitsu@gmail.com>
2015-03-02 20:56:04 +0100

    clean examples

--- index.php
+++ index.php
@@ -4,27 +4,7 @@
 
 include_once 'core/bootstrap.php';
 
-/* GET APP INSTANCE */
-/*
 core_functions();
 core_autoloader();
-
-$blogApp = App::getInstance('blog');
-$blogApp->setAsDefault();
-$blogApp->run();
-echo $blogApp;
-*/
-
-/* GET PLUGIN INSTANCE */
-/*
-core_functions();
-core_autoloader();
-
-$loader = Loader::getInstance();
-$loader->initClass('Plugin');
-
-$mailerPlugin = Plugin::getInstance('Mailer', array('smtp' => 'smtp.gmail.com'));
-
-*/
 
 ?>
