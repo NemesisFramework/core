9da189470d83765be548c95116642111b91af8ca
kimihub <kimhimitsu@gmail.com>
2015-04-07 20:45:54 +0200

    dev bootstrap

---
+++ nemesis.dev.php
@@ -0,0 +1,13 @@
+<?php
+/*
+	Bootstrap Dev
+*/
+
+
+// ERRORS
+error_reporting(E_ALL);
+ini_set('display_errors', 0);
+ini_set('log_errors', 'On');
+ini_set('error_log', NEMESIS_PATH.'errors.log');
+ini_set('ignore_repeated_errors', 'On');
+
