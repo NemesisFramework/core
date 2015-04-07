d30c44af6b721d208a41284ad59ad4dfa85e9cae
kimihub <kimhimitsu@gmail.com>
2015-04-07 23:47:27 +0200

    update

--- nemesis.dev.php
+++ nemesis.dev.php
@@ -6,8 +6,10 @@
 require_once 'nemesis.php';
 
 // ERRORS
+if (!file_exists(NEMESIS_PATH.'errors.log'))
+  touch(NEMESIS_PATH.'errors.log');
+
 error_reporting(E_ALL);
 ini_set('log_errors', true);
 ini_set('ignore_repeated_errors', true);
-touch(NEMESIS_PATH.'errors.log');
 ini_set('error_log', NEMESIS_PATH.'errors.log');
